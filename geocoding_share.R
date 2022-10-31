be90f139e10ab39adfa2f1bf7bdd35cc92304cc9
Yujin Kim <yujin71@gmail.com>
2022-10-30 22:19:49 -0500

    places trial
    _pa, _usa

--- geocoding_share.R
+++ geocoding_share.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(mapview)
 library(openxlsx)
 
-register_google(key = "XXX", write = TRUE)
+register_google(key = "AIzaSyD7caxQ0yruPRAJtCU26bJYR0WvqDp994w", write = TRUE)
 
 data <- read.csv("ner_place_freq_n5.csv")
 
@@ -33,8 +33,29 @@ data_merge <- data_merge %>% arrange(-desc(X))
 data_merge <- read.xlsx('data_merge.xlsx')
 data_merge_true <- data_merge %>% filter(is.na(address)==FALSE)
 #write.xlsx(data_merge, "data_merge.xlsx")
+data_merge_false <- data_merge %>% filter(is.na(address)==TRUE)
+
+##
+data <- data %>% mutate_geocode(location = unique_values, output = "more")
+saveRDS(data, "places_original.RDS")
+
+data_merge2 <- data %>% mutate(unique_values_pa = paste0(unique_values, ", PA, USA")) %>% select(X, unique_values_pa)
+data_merge2 <- data_merge2 %>% mutate_geocode(location = unique_values_pa, output = "more")
+saveRDS(data_merge2, "places_pa.RDS")
+
+colnames(data_merge2) <- paste(colnames(data_merge2), "pa", sep = "_")
+
+data_merge3 <- data %>% mutate(unique_values_usa = paste0(unique_values, ", USA")) %>% select(X, unique_values_usa)
+data_merge3 <- data_merge3 %>% mutate_geocode(location = unique_values_usa, output = "more")
+saveRDS(data_merge3, "places_usa.RDS")
+
+colnames(data_merge3) <- paste(colnames(data_merge3), "usa", sep = "_")
 
 
+data_merge_all <- cbind(data, data_merge2, data_merge3)
+
+data_merge_all_50 <- data_merge_all %>% sample_n(50)
+write.xlsx(data_merge_all_50, "google_api_places_50.xlsx")
 # view
 data_merge_true %>% 
   mapview(cex = "counts", xcol = "lon", ycol = "lat", crs = 4269, grid = FALSE)
